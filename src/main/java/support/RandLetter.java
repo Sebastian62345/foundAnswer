b88e61d8bd531ea703bb1035768f94bf93a76a85
Wozniak <wozniak500.com>
2023-06-29 20:12:32 +0200

    kontynuacja ukonczenia programu

--- src/main/java/support/RandLetter.java
+++ src/main/java/support/RandLetter.java
@@ -1,8 +1,17 @@
 package support;
-import org.apache.log4j.Logger;
+
 public class RandLetter {
+	byte min = 65; // Minimum value of range
+	byte max = 90; // Maximum value of range
+	byte asciCodeDec;
+	String randedLetter;
 	RandLetter() {
-		
+		asciCodeDec = (byte)Math.floor(Math.random() * (max - min + 1) + min);
+		randedLetter = new Character((char) asciCodeDec).toString();
+	}
+	
+	public String getRandedLetter(){
+		return this.randedLetter;
 	}
 	
 }
