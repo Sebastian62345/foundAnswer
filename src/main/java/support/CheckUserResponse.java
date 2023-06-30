be8e49409611ac0ec3c41e1b2df975c5fc1bd775
Wozniak <wozniak500.com>
2023-06-30 11:24:58 +0200

    kontynuacja prac nad interactionWithUser

--- src/main/java/support/CheckUserResponse.java
+++ src/main/java/support/CheckUserResponse.java
@@ -1,28 +1,57 @@
 package support;
-
+import java.util.*;
 public class CheckUserResponse {
 	
 	String userResponse;
 	private char questionType;
+	private RandNumber oN;
+	private RandLetter oL;
+	
 	
 	public CheckUserResponse(RandNumber v){
+		this.oN = v;
 		this.questionType = 'N';
 	}
 	
 	public CheckUserResponse(RandLetter v){
+		this.oL = v;
 		this.questionType = 'L';
 	}
 
 	public char interactionWithUser() {
+		Scanner getUserAnswerN = new Scanner(System.in);
 		char result = 'X';
+		int userNum;
+		int uResponse = 0;
 		if(questionType == 'N') {
+			int randedVal = oN.getRandedVal();
+			
 			System.out.print("Podaj liczbę o której Twoim zdaniem myślę: ");
-			result = 'Z';
+			while(true) {
+				uResponse = getUserAnswerN.nextInt();
+				if(uResponse == 0) {
+					System.out.println("Wybrano zero - program zostanie zakończony");
+					System.exit(0); 
+				}else if (uResponse > oN.getRandedVal()) {
+					System.out.println("Liczba o ktorej myślę jest mniejsza zgaduj dalej: ");
+				} else if (uResponse < oN.getRandedVal()) {
+					System.out.println("Liczba o ktorej myślę jest większa zgaduj dalej: ");
+				} else if (uResponse ==  oN.getRandedVal()) {
+					System.out.println("Brawo zgadłeś");
+					break;
+				}
+				//System.out.println("Wpisales " + uResponse);
+				result = 'Z';
+			
+			}
 		}
 		
 		if(questionType == 'L') {
 			System.out.print("Podaj literę o której Twoim zdaniem myślę: ");
+			Scanner getUserAnswerS = new Scanner(System.in);
+			System.out.println("Wpisales " + uResponse);
 			result =  'Z';
+			
 		}
 		return result;
 	}
--- src/main/java/ui/UI.java
+++ src/main/java/ui/UI.java
@@ -8,17 +8,17 @@ public class UI {
 	static Logger LObj = Logger.getLogger(UI.class);
 	public static void main(String[] args) {
 		String choice;
-		//LObj.debug("Start Game");
+		LObj.debug("Start Game");
 		System.out.println("Witaj w grze która polega na odgadnięciu litery albo cyfry która wylosuje:");
 		System.out.println("Wpisz cyfre 1 jeśli chcesz zgadnąć jaką liczbę wylosowałem z zakresu 0 - 100.");
 		System.out.println("Wpisz literę A jeśli chcesz zgadnąć jaką litere wylosowałem z zakresu A - Z.");
 		System.out.println("Wpisz cyfrę 0 w kazdym przypadku jesli chcesz zakończyć program.");
 		Scanner gud = new Scanner(System.in);
 		while(true) {
-			System.out.print("Wwój wybór to: ");
+			System.out.print("Twój wybór to: ");
 			choice = gud.nextLine();
 			if(choice.equals("0")) {
-				System.out.println("Program zostanie zakończony wybrano X");
+				System.out.println("Program zostanie zakończony wybrano 0");
 				break;
 			} else if (choice.equals("1") ) {
 				LObj.info("Wybrano losowanie liczby");
@@ -45,7 +45,7 @@ public class UI {
 			}
 			
 		}
-		//LObj.info("Finish Game");
+		LObj.info("Finish Game");
 		System.exit(0); 
 	}
 		
