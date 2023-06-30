a35eb2f7af2982706a9c9af2d3af902b4e4ffe3e
Wozniak <wozniak500.com>
2023-06-30 09:54:17 +0200

    kontynuacja pisania programu

--- src/main/java/support/CheckUserResponse.java
+++ src/main/java/support/CheckUserResponse.java
@@ -3,14 +3,19 @@ package support;
 public class CheckUserResponse {
 	
 	String userResponse;
-	char questionType;
+	private char questionType;
 	
 	public CheckUserResponse(RandNumber v){
 		this.questionType = 'N';
+	}
 	
 	public CheckUserResponse(RandLetter v){
 		this.questionType = 'L';
 	}
 
-	
+	public char interactionWithUser() {
+		if(questionType == 'N') {
+			System.out.print("podaj liczbę o której Twoim zdaniem myślę: ")
+		}
+	}
 }
